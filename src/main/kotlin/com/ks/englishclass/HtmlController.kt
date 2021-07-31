2030c7e7f7d042af6f4b23afa2243fb44b6b4e4e
ks <cks3443@gmail.com>
2021-08-01 01:26:40 +0900

    + jquery features

--- src/main/kotlin/com/ks/englishclass/HtmlController.kt
+++ src/main/kotlin/com/ks/englishclass/HtmlController.kt
@@ -1,5 +1,6 @@
 package com.ks.englishclass
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
@@ -7,11 +8,26 @@ import org.springframework.web.bind.annotation.GetMapping
 @Controller
 class HtmlController {
 
+  @Autowired
+  lateinit var sRepo: SentenceRepository
+
   @GetMapping("/")
   fun getIndexPage(
     model: Model
   ): String {
-    model.addAttribute("s1", "Hello World :)")
+    val ss = sRepo.getRandomOne()
+    model.addAttribute("s1", ss.s1)
+    model.addAttribute("s2", ss.s2)
+    model.addAttribute("isEditor", false)
     return "index"
   }
+
+  @GetMapping("/edit")
+  fun getEditPage(
+    model: Model
+  ): String {
+    model.addAttribute("isEditor", true)
+    return "index"
+  }
+
 }
--- src/main/kotlin/com/ks/englishclass/SentenceRepository.kt
+++ src/main/kotlin/com/ks/englishclass/SentenceRepository.kt
@@ -1,8 +1,12 @@
 package com.ks.englishclass
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface SentenceRepository : JpaRepository<Sentence, Long> {
+
+  @Query("SELECT * FROM SENTENCE ORDER BY rand() LIMIT 1", nativeQuery = true)
+  fun getRandomOne(): Sentence
 }
